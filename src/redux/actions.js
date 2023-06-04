fffe15acf8d2def8035063f8a0bb151ad04cef78
AmeyPank <pankantiameya@gmail.com>
2023-06-04 12:25:46 +0530

    React-Redux-Thunk-Social-Media

---
+++ src/redux/actions.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+// Action Types
+export const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST';
+export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
+export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
+
+// Action Creators
+export const fetchPostsRequest = () => ({
+    type: FETCH_POSTS_REQUEST,
+});
+
+export const fetchPostsSuccess = (posts) => ({
+    type: FETCH_POSTS_SUCCESS,
+    payload: posts,
+});
+
+export const fetchPostsFailure = (error) => ({
+    type: FETCH_POSTS_FAILURE,
+    payload: error,
+});
+
+// Thunk Function
+export const fetchPosts = () => {
+    return (dispatch) => {
+        dispatch(fetchPostsRequest());
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                const posts = response.data;
+                dispatch(fetchPostsSuccess(posts));
+            })
+            .catch((error) => {
+                dispatch(fetchPostsFailure(error.message));
+            });
+    };
+};
